ea94944e2196a8124c28e33b212dd9d3fa35e4cb
theBappy <abappy575@gmail.com>
2025-07-10 17:20:46 +0600

    upstash integration for redis and rate limit purpose

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,21 +1,20 @@
 import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+
 
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
       }),
     )
-    .query((opts) => {
-
-
-
+    .query((opts) => {   
+      console.log({dbUser: opts.ctx.user}) 
       return {
         greeting: `hello ${opts.input.text}`,
       };
     }),
 });
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
