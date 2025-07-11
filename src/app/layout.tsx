659d4f1d9647740c0b3345980b917a0f0f3045e9
theBappy <abappy575@gmail.com>
2025-07-11 19:11:38 +0600

    videos schema and create/post video route with trpc server-client prefetching technique

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from '@clerk/nextjs'
 import { TRPCProvider } from "@/trpc/client";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({
   subsets: ['latin']
@@ -25,6 +26,7 @@ export default function RootLayout({
         className={`${inter.className} antialiased`}
       >
         <TRPCProvider>
+          <Toaster />
           {children}
         </TRPCProvider>
       </body>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,14 @@
 
 import {categoriesRouter} from '@/modules/categories/server/procedure'
 import {  createTRPCRouter } from '../init';
+import { StudioRouter } from '@/modules/studio/server/procedures';
+import { videosRouter } from '@/modules/videos/server/procedures';
 
 
 export const appRouter = createTRPCRouter({
+  studio: StudioRouter,
   categories: categoriesRouter,
+  videos: videosRouter,
 });
 
 export type AppRouter = typeof appRouter;
