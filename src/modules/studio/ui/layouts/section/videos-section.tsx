0ecb44d6d6e89564606903822d282a7b1be9eb29
theBappy <abappy575@gmail.com>
2025-07-12 17:09:03 +0600

    infinite loading and also implement the table ui for video info

--- src/modules/studio/ui/layouts/section/videos-section.tsx
+++ src/modules/studio/ui/layouts/section/videos-section.tsx
@@ -1,20 +1,82 @@
-'use client'
+"use client";
 
-import { DEFAULT_LIMIT } from "@/constants"
-import { trpc } from "@/trpc/client"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { DEFAULT_LIMIT } from "@/constants";
+import { InfiniteScroll } from "@/modules/studio/components/infinite-scroll";
+import { trpc } from "@/trpc/client";
+import Link from "next/link";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export const VideosSection = () => {
-  const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
-    limit: DEFAULT_LIMIT,
-  }, {
-    getNextPageParam: (lastPage) => lastPage.nextCursor,
-  })
-  
-  
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+export const VideosSectionSuspense = () => {
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
   return (
     <div>
-       {JSON.stringify(data)} 
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <Link legacyBehavior href={`/studio/videos/${video.id}`} key={video.id}>
+                <TableRow
+                  className="cursor-pointer"
+                >
+                  <TableCell>{video.title}</TableCell>
+                  <TableCell>Visibility</TableCell>
+                  <TableCell>Status</TableCell>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Views</TableCell>
+                  <TableCell>Comments</TableCell>
+                  <TableCell>Likes</TableCell>
+                </TableRow>
+                </Link>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+      <InfiniteScroll
+        isManual
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
     </div>
-  )
-}
-
+  );
+};
