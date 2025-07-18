b3a81fbe197260be9323f10adb1e1f0b9d825dfc
theBappy <abappy575@gmail.com>
2025-07-18 19:07:41 +0600

    comment schema and relation with video and user api

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,6 +5,7 @@ import { categoriesRouter } from "@/modules/categories/server/procedure";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
+import { commentsRouter } from "@/modules/comments/server/procedures";
 
 export const appRouter = createTRPCRouter({
   studio: StudioRouter,
@@ -13,6 +14,7 @@ export const appRouter = createTRPCRouter({
   videoViews: videoViewsRouter,
   videoReactions: videoReactionsRouter,
   subscriptions: subscriptionsRouter,
+  comments: commentsRouter,
 });
 
 export type AppRouter = typeof appRouter;
