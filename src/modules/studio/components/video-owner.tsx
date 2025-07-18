68a018531c58564e357691dc05e01b0e611df514
theBappy <abappy575@gmail.com>
2025-07-18 18:34:32 +0600

    subscribe api also unsubscribe implementation

--- src/modules/studio/components/video-owner.tsx
+++ src/modules/studio/components/video-owner.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { UserAvatar } from "@/components/user-avatar";
+import { useSubscriptions } from "@/modules/subscriptions/hooks/use-subscription";
 import { SubscriptionButton } from "@/modules/subscriptions/subscription-button";
 import { VideoGetOneOutput } from "@/modules/types";
 import { UserInfo } from "@/modules/users/user-info";
@@ -12,7 +13,12 @@ interface VideoOwnerProps {
 }
 
 export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
-  const { userId: clerkUserId } = useAuth();
+  const { userId: clerkUserId, isLoaded } = useAuth();
+  const {isPending, onClick} = useSubscriptions({
+    userId: user.id,
+    isSubscribed: user.viewerSubscribed,
+    fromVideoId: videoId,
+  })
 
   return (
     <div className="flex items-center sm:items-start justify-between sm:justify-start gap-3">
@@ -23,7 +29,7 @@ export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
           <UserInfo size='lg' name={user.name} />
           <span className="text-sm text-muted-foreground line-clamp-1">
             {/* todo: properly fill subscriber counts */}
-            {0} subscribers
+            {user.subscriberCount} subscribers
           </span>
           </div>
         </div>
@@ -34,9 +40,9 @@ export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
         </Button>
       ) : (
         <SubscriptionButton
-          onClick={() => {}}
-          disabled={false}
-          isSubscribed={false}
+          onClick={onClick}
+          disabled={isPending || isLoaded}
+          isSubscribed={user.viewerSubscribed}
           className="flex-none"
         />
       )}
---
+++ src/modules/subscriptions/hooks/use-subscription.ts
@@ -0,0 +1,60 @@
+import {toast} from 'sonner'
+import {useClerk} from '@clerk/nextjs'
+import {trpc} from '@/trpc/client'
+
+interface UseSubscriptionsProps {
+    userId: string;
+    isSubscribed: boolean;
+    fromVideoId?: string;
+}
+
+export const useSubscriptions = ({userId, isSubscribed, fromVideoId}:UseSubscriptionsProps) => {
+    const clerk = useClerk()
+    const utils = trpc.useUtils()
+
+    const subscribe = trpc.subscriptions.create.useMutation({
+        onSuccess: () => {
+            toast.success('Subscribed')
+            // todo: re-invalidate subscriptions.getMany, users.getOne
+            if(fromVideoId){
+                utils.videos.getOne.invalidate({id: fromVideoId})
+            }
+        },
+        onError: (error) => {
+            toast.error('Something went wrong')
+            if(error.data?.code === 'UNAUTHORIZED'){
+                clerk.openSignIn()
+            }
+        }
+    })
+    const unsubscribe = trpc.subscriptions.remove.useMutation({
+        onSuccess: () => {
+            toast.success('Unsubscribed')
+            // todo: re-invalidate subscriptions.getMany, users.getOne
+            if(fromVideoId){
+                utils.videos.getOne.invalidate({id: fromVideoId})
+            }
+        },
+        onError: (error) => {
+            toast.error('Something went wrong')
+            if(error.data?.code === 'UNAUTHORIZED'){
+                clerk.openSignIn()
+            }
+        }
+    })
+
+    const isPending = subscribe.isPending || unsubscribe.isPending;
+
+    const onClick = () => {
+        if(isSubscribed){
+            unsubscribe.mutate({userId})
+        }else{
+            subscribe.mutate({userId})
+        }
+    }
+
+    return {
+        isPending, 
+        onClick,
+    }
+}
