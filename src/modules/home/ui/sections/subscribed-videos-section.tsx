f63e008c4f250d04f33ecd072b78cf912f7ea670
theBappy <abappy575@gmail.com>
2025-07-23 16:23:54 +0600

    only subscribed videos fetching API

---
+++ src/modules/home/ui/sections/subscribed-videos-section.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { DEFAULT_LIMIT } from "@/constants";
+import { InfiniteScroll } from "@/modules/studio/components/infinite-scroll";
+import { VideoGridCard, VideoGridCardSkeleton } from "@/modules/studio/ui/video-grid-card";
+import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export const SubscribedVideosSection = () => {
+  return (
+    <Suspense fallback={<SubscribedVideoSectionSkeleton />}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <SubscribedVideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const SubscribedVideoSectionSkeleton = () => {
+  return <div className="gap-4 gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5 [@media(min-width:2200px)]:grid-cols-6">
+      {Array.from({length: 18}).map((_, index) => (
+        <VideoGridCardSkeleton key={index} />
+      ))}
+    </div>
+};
+
+const SubscribedVideosSectionSuspense = () => {
+  const [videos, query] = trpc.videos.getManySubscribed.useSuspenseInfiniteQuery(
+    { limit: DEFAULT_LIMIT },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+  return (
+    <div>
+    <div className="gap-4 gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5 [@media(min-width:2200px)]:grid-cols-6">
+      {videos.pages
+        .flatMap((page) => page.items)
+        .map((video) => (
+            <VideoGridCard key={video.id} data={video} />
+        ))
+        }
+    </div>
+    <InfiniteScroll
+    hasNextPage={query.hasNextPage}
+    isFetchingNextPage={query.isFetchingNextPage}
+    fetchNextPage={query.fetchNextPage}
+    />
+    </div>
+  );
+};
