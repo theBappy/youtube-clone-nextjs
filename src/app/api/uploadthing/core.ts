2f1406355a724ebcb50d1f61fcacc8f2199b0d6e
theBappy <abappy575@gmail.com>
2025-07-25 09:37:09 +0600

    banner upload api for user profile

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -9,6 +9,50 @@ import { videos, users } from "@/db/schema";
 const f = createUploadthing();
 
 export const ourFileRouter = {
+  bannerUploader: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  })
+    .middleware(async () => {
+      const { userId: clerkUserId } = await auth();
+
+      if (!clerkUserId) throw new UploadThingError("Unauthorized");
+
+      const [existingUser] = await db
+        .select()
+        .from(users)
+        .where(eq(users.clerkId, clerkUserId));
+
+      if (!existingUser) throw new UploadThingError("Unauthorized");
+
+      if (existingUser.bannerKey) {
+        const utapi = new UTApi();
+
+        await utapi.deleteFiles(existingUser.bannerKey);
+        await db
+          .update(users)
+          .set({ bannerKey: null, bannerUrl: null })
+          .where(and(eq(users.id, existingUser.id)));
+      }
+
+      return { userId: existingUser.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      await db
+        .update(users)
+        .set({
+          bannerUrl: file.url,
+          bannerKey: file.key,
+        })
+        .where(eq(users.id, metadata.userId));
+
+      return { uploadedBy: metadata.userId };
+    }),
+
+    
+
   thumbnailUploader: f({
     image: {
       maxFileSize: "4MB",
@@ -37,25 +81,19 @@ export const ourFileRouter = {
           thumbnailKey: videos.thumbnailKey,
         })
         .from(videos)
-        .where(and(
-          eq(videos.id, input.videoId),
-          eq(videos.userId, user.id),
-        ))
+        .where(and(eq(videos.id, input.videoId), eq(videos.userId, user.id)));
 
-        if(!existingVideo) throw new UploadThingError("Not found");
+      if (!existingVideo) throw new UploadThingError("Not found");
 
-        if(existingVideo.thumbnailKey){
-          const utapi = new UTApi()
+      if (existingVideo.thumbnailKey) {
+        const utapi = new UTApi();
 
-          await utapi.deleteFiles(existingVideo.thumbnailKey)
-          await db
-            .update(videos)
-            .set({thumbnailKey: null, thumbnailUrl: null})
-            .where(and(
-              eq(videos.id, input.videoId),
-              eq(videos.userId, user.id),
-            ))
-        }
+        await utapi.deleteFiles(existingVideo.thumbnailKey);
+        await db
+          .update(videos)
+          .set({ thumbnailKey: null, thumbnailUrl: null })
+          .where(and(eq(videos.id, input.videoId), eq(videos.userId, user.id)));
+      }
 
       return { user, ...input };
     })
--- src/modules/users/user-banner.tsx
+++ src/modules/users/user-banner.tsx
@@ -4,6 +4,8 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Edit2Icon } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { BannerUploadModal } from "./banner-upload-modal";
+import { useState } from "react";
 
 interface UserPageBannerProps {
   user: UserGetOneOutput;
@@ -16,10 +18,18 @@ export const UserPageBannerSkeleton = () => {
 }
 
 export const UserPageBanner = ({ user }: UserPageBannerProps) => {
+
   const { userId } = useAuth();
+  const [isBannerUploadModalOpen, setIsBannerUploadModalOpen] = useState(false)
+
+
   return (
     <div className="relative group">
-        {/* TODO: add upload banner modal */}
+        <BannerUploadModal 
+        userId={user.id}
+        open={isBannerUploadModalOpen}
+        onOpenChange={setIsBannerUploadModalOpen}
+        />
       <div
         className={cn(
           "w-full max-h-[200px] h-[15vh] md:h-[25vh] bg-gradient-to-r from-gray-100 to-gray-100 rounded-xl",
@@ -33,6 +43,7 @@ export const UserPageBanner = ({ user }: UserPageBannerProps) => {
       >
         {user.clerkId === userId && (
           <Button
+            onClick={() => setIsBannerUploadModalOpen(true)}
             type="button"
             size="icon"
             className="absolute top-4 right-4 rounded-full bg-black/50 opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity duration-300 hover:bg-black/50"
